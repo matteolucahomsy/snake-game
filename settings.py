ef1b4bc5a7f28c12581f06b6f51439a147a758ef
matteolucahomsy <matteoluca.homsy@net.usj.edu.lb>
2026-05-19 16:27:43 +0300

    Initial project setup

---
+++ settings.py
@@ -0,0 +1,10 @@
+import pygame
+pygame.init()
+screen=pygame.display.set_mode((600,600))
+running=True
+while running:
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            running= False
+    pygame.display.update()
+pygame.quit()
